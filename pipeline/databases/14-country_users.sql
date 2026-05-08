8cb4b9c17e02116243a3dc9e031218769f95534b
fs-dlh <faruk.sari@ai.dlh.lu>
2026-05-08 10:23:24 +0200

    task 14 commit

--- pipeline/databases/14-country_users.sql
+++ pipeline/databases/14-country_users.sql
@@ -1,12 +1,14 @@
--- 13-uniq_users.sql
+-- 14-country_users.sql
 -- Creates a table users with
 --     id (auto increment, primary key),
 --     email (unique, not null),
---     and name
-
+--     name,
+--     and country enumeration (US, CO, TN)
+-- The country column has a default value of 'US' and cannot be NULL
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
     PRIMARY KEY (id)
 );
