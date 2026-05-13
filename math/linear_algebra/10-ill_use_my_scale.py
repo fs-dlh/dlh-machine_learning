fcc6fa78723cdb276bf14261dadbf80fae705d4a
fs-dlh <faruk.sari@ai.dlh.lu>
2026-05-13 13:44:19 +0200

    task 10 commit

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module for getting the shape of a numpy ndarray."""
+
+
+def np_shape(matrix):
+    """
+    Return the shape of a numpy.ndarray as a tuple of integers.
+
+    Args:       matrix (numpy.ndarray): Input array.
+    Returns:    tuple: Shape of the array.
+    """
+    return matrix.shape
