f83518c2d7ea11539b7768f47f8952be0f300eba
fs-dlh <faruk.sari@ai.dlh.lu>
2026-05-13 13:47:11 +0200

    task 11 commit

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module for transposing a numpy ndarray."""
+
+
+def np_transpose(matrix):
+    """
+    Return the transpose of a numpy.ndarray.
+
+    Args:       matrix (numpy.ndarray): Input array.
+    Returns:    numpy.ndarray: Transposed array.
+    """
+    return matrix.T
