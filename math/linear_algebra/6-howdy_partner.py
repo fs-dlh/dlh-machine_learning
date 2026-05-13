2f82e8439b088a17443c950b137e501075d406a1
fs-dlh <faruk.sari@ai.dlh.lu>
2026-05-13 09:07:28 +0200

    task 6 commit

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for concatenating two arrays."""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenate two arrays.
+
+    Args:
+        arr1 (list of int/float): First array.
+        arr2 (list of int/float): Second array.
+
+    Returns:
+        list: A new list containing all elements of arr1 followed by arr2.
+    """
+    return arr1 + arr2
