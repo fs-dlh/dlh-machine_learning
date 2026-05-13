5f49eb39fd2aa51159f23b555ab098e6fcbb39e5
fs-dlh <faruk.sari@ai.dlh.lu>
2026-05-13 09:02:34 +0200

    task 5 commit

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -19,4 +19,8 @@ def add_matrices2D(mat1, mat2):
     for i in range(len(mat1)):
         if len(mat1[i]) != len(mat2[i]):
             return None
-    return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[i]))] for i in range(len(mat1))]
+    return [
+        [mat1[i][j] + mat2[i][j]
+        for j in range(len(mat1[i]))]
+            for i in range(len(mat1))
+    ]
