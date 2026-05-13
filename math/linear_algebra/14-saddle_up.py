45d90723dc0fc182daee846bd1a89ab00cb9a4fe
fs-dlh <faruk.sari@ai.dlh.lu>
2026-05-13 13:55:01 +0200

    task 14 commit

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module for matrix multiplication using numpy."""
+
+
+import numpy as np
+
+def np_matmul(mat1, mat2):
+    """
+    Multiply two numpy ndarrays using matrix multiplication.
+
+    Args:
+        mat1 (numpy.ndarray): First matrix.
+        mat2 (numpy.ndarray): Second matrix.
+    Returns:
+        numpy.ndarray: Result of matrix multiplication.
+    """
+    return np.matmul(mat1, mat2)
