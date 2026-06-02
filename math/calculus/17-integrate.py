24c60c0a5f3618e703adfc8f133657f24e453b55
fs-dlh <faruk.sari@ai.dlh.lu>
2026-06-02 20:23:07 +0200

    Calculus Task17 commit

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -13,25 +13,22 @@ def poly_integral(poly, C=0):
 
     if not isinstance(poly, list) or not isinstance(C, int):
         return None
-
+    
     for coeff in poly:
         if not isinstance(coeff, (int, float)):
             return None
-
-    if poly == []:
-        return [C]
-
+    
     result = [C]
 
     for i, coeff in enumerate(poly):
-        val = coeff / (i + 1)
+        val = coeff // (i + 1)
 
         if val.is_integer():
             val = int(val)
 
         result.append(val)
 
-    while len(result) > 0 and result[-1] == 0:
+    while len(result) > 1 and result[-1] == 0:
         result.pop()
 
     return result
