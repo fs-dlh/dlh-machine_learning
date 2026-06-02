0d1ec1c15ef23775d9e51b2445fca7f3fb239644
fs-dlh <faruk.sari@ai.dlh.lu>
2026-06-02 22:27:03 +0200

    Plotting Task 4 commit

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -8,11 +8,18 @@ def frequency():
     """Plot a histogram of student scores."""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
+
     plt.figure(figsize=(6.4, 4.8))
-    plt.hist(student_grades, bins=10, range=(0, 100), edgecolor='black')
+
+    plt.hist(student_grades,
+             bins=np.arange(0, 101, 10),
+             edgecolor='black')
+
     plt.xticks(np.arange(0, 101, 10))
     plt.xlim(0, 100)
+
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
+
     plt.show()
