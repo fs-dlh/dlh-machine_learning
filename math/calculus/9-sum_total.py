fe34cd4c6da197f5eaa13b6d858b4a93fd1bfdaf
fs-dlh <faruk.sari@ai.dlh.lu>
2026-06-02 16:19:02 +0200

    Calculus Task9 commit

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,10 +5,9 @@ the sum of all squared natural numbers up to n.
 
 
 def summation_i_squared(n):
-    if not isinstance(n, int) :
+    if not isinstance(n, int):
         return None
     if n > 0:
         return n**2 + summation_i_squared(n-1)
-    else
+    else:
         return 0
-    
