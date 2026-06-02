1f1ae8668a2ea0a926ba8e8b3980e04655b12458
fs-dlh <faruk.sari@ai.dlh.lu>
2026-06-02 21:33:06 +0200

    Plotting Task 0 commit

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -9,7 +9,7 @@ def line():
     y = np.arange(0, 11) ** 3
     x = np.arange(0, 11)
 
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(6.4, 4.8))
     plt.plot(x, y, 'r-')
     plt.xlim(0, 10)
     plt.show()
