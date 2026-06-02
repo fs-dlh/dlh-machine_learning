fffc5f7ed9116e7b697e034b55d0a3c49d301688
fs-dlh <faruk.sari@ai.dlh.lu>
2026-06-02 17:37:38 +0200

    Calculus Task15-17 commit

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Calculates the integral of a polynomial."""
+
+
+def poly_integral(poly):
+    """    Calculates the integral of a polynomial.
+
+    Args:
+        poly: A list of coefficients representing the polynomial.
+
+    Returns:
+        A list of coefficients as the integral of the polynomial."""
+
+    if not isinstance(poly, list):
+        return None
+
+    if len(poly) == 1:
+        return [0]
+
+    integral = [0]
+    for i in range(len(poly)):
+        integral.append(poly[i] / (i + 1))
+
+    return integral
