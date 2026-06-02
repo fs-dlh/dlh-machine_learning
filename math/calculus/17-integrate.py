44eea040c73b2f5bc1e8aa9b784d38c5ed16c52a
fs-dlh <faruk.sari@ai.dlh.lu>
2026-06-02 19:16:24 +0200

    Calculus Task17 commit

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -11,7 +11,8 @@ def poly_integral(poly, C=0):
     Returns:
         A list of coefficients as the integral of the polynomial."""
 
-    if not isinstance(poly, list) or not isinstance(C, (int, float)):
+    if not isinstance(poly, list) or not isinstance(C, (int, float)) \
+            or not poly:
         return None
     for coeff in poly:
         if not isinstance(coeff, (int, float)):
