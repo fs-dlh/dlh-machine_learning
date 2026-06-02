9fbff5ecf12ee2e0239800545aff1117d13f97f5
fs-dlh <faruk.sari@ai.dlh.lu>
2026-06-02 21:48:35 +0200

    Plotting Task 1 commit

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -12,8 +12,8 @@ def scatter():
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
     plt.figure(figsize=(6.4, 4.8))
-    plt.scatter(x, y, c='m', s=10)
+    plt.scatter(x, y, c='magenta')
     plt.xlabel('Height (in)')
     plt.ylabel('Weight (lbs)')
-    plt.title('Men\'s Height vs Weight')
+    plt.title("Men's Height vs Weight")
     plt.show()
