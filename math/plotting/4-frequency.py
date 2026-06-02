811f5d897c34ac19291eb5c0c15c179af0d30c09
fs-dlh <faruk.sari@ai.dlh.lu>
2026-06-02 22:30:08 +0200

    Plotting Task 4 commit

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -12,14 +12,13 @@ def frequency():
     plt.figure(figsize=(6.4, 4.8))
 
     plt.hist(student_grades,
-             bins=np.arange(0, 101, 10),
+             bins=np.arange(0, 110, 10),
              edgecolor='black')
 
-    plt.xticks(np.arange(0, 101, 10))
     plt.xlim(0, 100)
-
+    plt.xticks(np.arange(0, 110, 10))
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-
+    plt.tight_layout()
     plt.show()
