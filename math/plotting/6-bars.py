7fa32627089297ce9a7dc7009ea61abfc247a004
fs-dlh <faruk.sari@ai.dlh.lu>
2026-06-03 11:46:51 +0200

    Plotting Task 6 commit

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -8,7 +8,7 @@ def bars():
     """Plots a stacked bar graph of fruit counts."""
 
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
 
     # your code here
@@ -16,13 +16,13 @@ def bars():
     fruit_names = ['apples', 'bananas', 'oranges', 'peaches']
     colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
     persons = ['Farrah', 'Fred', 'Felicia']
-    x = np.arange(len(persons)) 
+    x = np.arange(len(persons))
     width = 0.5
     bottom = np.zeros(len(persons))
     for i in range(len(fruit_names)):
         plt.bar(x, fruit[i], width=width, bottom=bottom,
                 color=colors[i], label=fruit_names[i])
-        bottom += fruit[i]  
+        bottom += fruit[i]
     plt.ylabel('Quantity of Fruit')
     plt.title('Number of Fruit per Person')
     plt.ylim(0, 80)
