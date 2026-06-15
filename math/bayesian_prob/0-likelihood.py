8692411e1edba365222c37c1a41d486a5f2cb732
fs-dlh <faruk.sari@ai.dlh.lu>
2026-06-15 11:48:16 +0200

    Bayesian Task 0 commit

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -56,7 +56,7 @@ def likelihood(x, n, P):
     """
 
     if not (isinstance(n, int) and ( n > 0  )):
-        raise TypeError("n must be a positive integer") 
+        raise ValueError("n must be a positive integer") 
 
     if not (isinstance(x, int) and ( x >= 0)):
         raise ValueError("x must be an integer that is greater than or equal to 0")
