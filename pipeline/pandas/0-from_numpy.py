6587b6add5c54070d44e61979a09b642e93b54bd
fs-dlh <faruk.sari@ai.dlh.lu>
2026-07-12 14:30:30 +0200

    pandas task 0 commit

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
 
+""" Module for creating a pandas DataFrame from a numpy array. """
+
+import pandas as pd
+
+
+def from_numpy(array):
+    """ Creates a pd.DataFrame from a np.ndarray.
+
+    Args:
+        array (np.ndarray): The numpy array to convert.
+
+    Returns:
+        pd.DataFrame: The newly created DataFrame with columns labeled
+    """
+    num_cols = array.shape[1]
+    cols = list(chr(65 + i) for i in range(num_cols))
+    return pd.DataFrame(array, columns=cols)
