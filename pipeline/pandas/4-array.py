631bd4ef6e23d81a09798e3a45718f3a8ac6668c
fs-dlh <faruk.sari@ai.dlh.lu>
2026-07-12 16:06:02 +0200

    pandas task 4 commit

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Module to extract last 10 rows of High and Close columns as numpy array.
+"""
+
+import pandas as pd
+
+
+def array(df):
+    """
+    Selects the last 10 rows of the 'High' and 'Close' columns and converts
+    them to a numpy.ndarray.
+
+    Args:
+        df (pd.DataFrame): Input DataFrame contains 'High' and 'Close' columns.
+
+    Returns:
+        np.ndarray: The selected values as a numpy array.
+    """
+
+    return df[['High', 'Close']].tail(10).values
