7593b647d744a4182571b31940c2ab3aa0fd4d38
fs-dlh <faruk.sari@ai.dlh.lu>
2026-08-12 20:00:29 +0200

    Keras Task12 commit

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -7,4 +7,4 @@ def test_model(network, data, labels, verbose=True):
     """ Tests a neural network by evaluating it on provided data and labels."""
     verbose_flag = 1 if verbose else 0
     loss, accuracy = network.evaluate(data, labels, verbose=verbose_flag)
-    return loss, accuracy
+    return [loss, accuracy]
