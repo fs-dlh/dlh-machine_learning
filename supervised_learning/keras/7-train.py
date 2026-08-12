567297779b76609e81ace340eca64657cb13572d
fs-dlh <faruk.sari@ai.dlh.lu>
2026-08-12 18:19:22 +0200

    Keras Task7 commit

--- supervised_learning/keras/7-train.py
+++ supervised_learning/keras/7-train.py
@@ -5,7 +5,6 @@ import tensorflow.keras as K
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False, patience=0,
-                learning_rate_decay=False, alpha=0.1, decay_rate=1,
                 verbose=True, shuffle=False):
     """ Trains a model using mini-batch gradient.
 
