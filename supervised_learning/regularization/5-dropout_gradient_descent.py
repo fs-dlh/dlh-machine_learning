5c62e934675133ae70181236a73980bb0507c076
fs-dlh <faruk.sari@ai.dlh.lu>
2026-09-06 19:23:06 +0200

    Task 5

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -17,7 +17,6 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
 
     m = Y.shape[1]
-
     dZ = cache['A' + str(L)] - Y
 
     for i in range(L, 0, -1):
@@ -27,15 +26,9 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
         dW = (1 / m) * np.matmul(dZ, A_prev.T)
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
+        if i > 1:
+            dA_prev = np.matmul(W.T, dZ) * cache['D' + str(i - 1)] / keep_prob
+            dZ = dA_prev * (1 - np.power(A_prev * keep_prob, 2))
+
         weights['W' + str(i)] -= alpha * dW
         weights['b' + str(i)] -= alpha * db
-
-        if i > 1:
-            dA_prev = np.matmul(W.T, dZ)
-            dA_prev *= cache['D' + str(i - 1)] / keep_prob
-
-            A_prev_drop = cache['A' + str(i - 1)]
-
-            A_raw = A_prev_drop * keep_prob
-
-            dZ = dA_prev * (1 - np.power(A_raw, 2))
