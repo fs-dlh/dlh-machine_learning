2b57b424cc6a82c28e23d934143b5fd48fb79216
fs-dlh <faruk.sari@ai.dlh.lu>
2026-09-06 19:09:52 +0200

    Task 5

---
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" Gradient descent with Dropout regularization for a neural network. """
+import numpy as np
+
+
+def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
+    """ Update weights using gradient descent with Dropout.
+
+    Args:
+        Y : one‑hot labels of shape (classes, m).
+        weights : contains 'W1','b1',...,'WL','bL'.
+        cache : from dropout_forward_prop, contains 'A0'..'AL'
+                      and 'D1'..'D{L-1}'.
+        alpha : learning rate.
+        keep_prob : probability of keeping a node.
+        L : number of layers.
+    """
+
+    m = Y.shape[1]
+
+    dZ = cache['A' + str(L)] - Y
+
+    for i in range(L, 0, -1):
+        A_prev = cache['A' + str(i - 1)]
+        W = weights['W' + str(i)]
+
+        dW = (1 / m) * np.matmul(dZ, A_prev.T)
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+
+        weights['W' + str(i)] -= alpha * dW
+        weights['b' + str(i)] -= alpha * db
+
+        if i > 1:
+            dA_prev = np.matmul(W.T, dZ)
+            dA_prev *= cache['D' + str(i - 1)] / keep_prob
+
+            A_prev_drop = cache['A' + str(i - 1)]
+
+            A_raw = A_prev_drop * keep_prob
+
+            dZ = dA_prev * (1 - np.power(A_raw, 2))
