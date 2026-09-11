4b34ded24bb4f221701250934af6fb44fb36bfa9
fs-dlh <faruk.sari@ai.dlh.lu>
2026-09-11 16:59:48 +0200

    Task 4/5

---
+++ pipeline/data_augmentation/4-brightness.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Module for randomly changing the brightness of an image using TF. """
+import tensorflow as tf
+
+
+def change_brightness(image, max_delta):
+    """Randomly change the brightness of an image.
+
+    Args:
+        image: A 3D tf.Tensor containing the image to change.
+        max_delta: The maximum amount the image should be brightened.
+
+    Returns:
+        The brightness-altered image as a tf.Tensor.
+    """
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image = tf.image.random_brightness(image, max_delta)
+    return tf.image.convert_image_dtype(image, tf.uint8)
