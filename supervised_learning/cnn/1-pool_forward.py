09ea955510837bbea632d20048c14d34bc5853d3
fs-dlh <faruk.sari@ai.dlh.lu>
2026-09-11 11:51:45 +0200

    Task 1

--- supervised_learning/cnn/1-pool_forward.py
+++ supervised_learning/cnn/1-pool_forward.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-"""Pooling forward propagation module."""
-
+""" Pooling forward propagation module. """
 import numpy as np
 
 
 def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
-    """
-    Performs forward propagation over a pooling layer of a neural network.
+    """ Performs forward propagation over a pooling layer of a neural network.
 
     Args:
         A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
@@ -21,26 +19,20 @@ def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw = kernel_shape
     sh, sw = stride
-
     out_h = (h_prev - kh) // sh + 1
     out_w = (w_prev - kw) // sw + 1
-
     A = np.zeros((m, out_h, out_w, c_prev))
-
     for i in range(out_h):
         h_start = i * sh
         h_end = h_start + kh
         for j in range(out_w):
             w_start = j * sw
             w_end = w_start + kw
-
             A_slice = A_prev[:, h_start:h_end, w_start:w_end, :]
-
             if mode == 'max':
                 A[:, i, j, :] = np.max(A_slice, axis=(1, 2))
             elif mode == 'avg':
                 A[:, i, j, :] = np.mean(A_slice, axis=(1, 2))
             else:
                 raise ValueError("mode must be 'max' or 'avg'")
-
     return A
